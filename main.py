3519fead703ea4dfce725e6c8089db92d5ad04f9
Aman Mishra <34169960+amanaation@users.noreply.github.com>
2021-08-10 13:38:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# import os
+# # Use the package we installed
+# from slack_bolt import App
+#
+# # Initializes your app with your bot token and signing secret
+#
+#
+# # Add functionality here
+# # @app.event("app_home_opened") etc
+#
+#
+# @app.message("hello")
+# def say_hello(message, say):
+#     user = message['user']
+#     say(f"Hi there, <@{user}>!")
+#
+#
+# # Start your app
+# if __name__ == "__main__":
+#     app.start(port=int(os.environ.get("PORT", 3000)))
+
+
+print(" starting code")
+
+print(1997 + 1997 + 1997 + 1996 + 1998)
+
+print(" Ending code")
